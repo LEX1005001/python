3902d9951f1f61bfd0e307599abc9b2a0d43d83c
LEX1005001 <AleX69.galkin@yandex.ru>
2022-11-03 20:17:46 +0300

    4

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-print("Что ?")
-print("ДА")
+num=int(input("Введите число: "))
+sum=0 # Cумама чисел
+count=0
+count0=0
+while num!=0:
+    d=num%10
+    num//10
+    count+=1
+    sum+=d
+if d==0:
+    count0+=1
+print("Сумма чисел: ",sum)
+print("Колличество чисел: ",count)
+print("Среднее арифм: ",sum/count)
+print("Колличество нулей в числе: ",count0)
