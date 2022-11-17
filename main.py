6c79efd7a64ece297369c78a92f844ed59ebb3aa
LEX1005001 <AleX69.galkin@yandex.ru>
2022-11-17 21:12:39 +0300

    0

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-num=int(input("Введите число: "))
-sum=0 # Cумама чисел
-count=0
-count0=0
-while num!=0:
-    d=num%10
-    num//10
-    count+=1
-    sum+=d
-    if d==0:
-        count0+=1
-print("Сумма чисел: ",sum)
-print("Колличество чисел: ",count)
-print("Среднее арифм: ",sum/count)
-print("Колличество нулей в числе: ",count0)
+from random import(randint)
+
+matrix=[]
+size=4
+for i in range(size):
+    row=[]
+    for j in range(size):
+        row.append(randint(0,10))
+    matrix.append(row)
+for i in range (size):
+    for j in range(size):
+        print(matrix[i][j],end=" ")
+    print()
+print("Среднее арифм: ")
+for i in range(size):
+    print((int(sum(matrix[i])))/size) #Cреднее арифм
+print("Колл-во 0 в строке: ")
+for i in range(size):
+    print(matrix.count(0))
